5e1fcc448f2685472e1d8796959fe90a9b0c97b1
Fedor Nezhivoi <f.nezhivoi@corp.vk.com>
2025-03-31 19:38:02 +0700

    svelte/no-unused-props repro

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Bug1 from './Bug1.svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Bug1 test="foo" />
